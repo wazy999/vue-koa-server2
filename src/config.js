9a2252b1aba5ed1601dc2a4f86521050cb8ecc11
fanghaoji <1710336112@qq.com>
2020-11-23 23:25:44 +0800

    #fix: 设置默认数据库为云数据库

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export default {
   yuqueToken: 'qQMNC7DWhyRsHrYmxVmKf1KgFEe0RcOM9YpgDSh7',
   docsUrl: 'api/v2/repos/mijixunzong/kb/docs/',
   baseURL: 
-  // 'http://120.79.115.240:5000',
-  'http://localhost:5000',
+  'http://120.79.115.240:5000',
+  // 'http://localhost:5000',
   authorsAndBooks: [
     {
       author: "mijixunzong",
